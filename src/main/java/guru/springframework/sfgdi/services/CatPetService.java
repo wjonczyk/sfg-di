83154506fc68897ddfefe204578496e33384ca13
Wojciech Jończyk <wjonczyk@duck.com>
2022-10-18 19:01:13 +0200

    fixing DI problems for assignment review

--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -1,8 +1,10 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service("cat")
+@Service
+@Profile("cat")
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,9 +1,11 @@
 package guru.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
+@Service
 @Profile({"dog", "default"})
-public class DogPetService {
+public class DogPetService implements PetService {
     public String getPetType(){
         return "Dogs are the best!";
     }
